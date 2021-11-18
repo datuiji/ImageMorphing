08c8a5081ec2d13408f96b6c8770acb0b12ca0a6
楊鴻志 <s197601202000@gmail.com>
2021-11-18 20:15:46 +0800

    remove build directory from .gitignore

---
+++ build/CMakeFiles/ImageMorphing.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/JUCE/modules/juce_core/juce_core.mm" "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/build/CMakeFiles/ImageMorphing.dir/JUCE/modules/juce_core/juce_core.mm.o"
+  "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/JUCE/modules/juce_data_structures/juce_data_structures.mm" "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/build/CMakeFiles/ImageMorphing.dir/JUCE/modules/juce_data_structures/juce_data_structures.mm.o"
+  "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/JUCE/modules/juce_events/juce_events.mm" "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/build/CMakeFiles/ImageMorphing.dir/JUCE/modules/juce_events/juce_events.mm.o"
+  "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/JUCE/modules/juce_graphics/juce_graphics.mm" "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/build/CMakeFiles/ImageMorphing.dir/JUCE/modules/juce_graphics/juce_graphics.mm.o"
+  "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/JUCE/modules/juce_gui_basics/juce_gui_basics.mm" "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/build/CMakeFiles/ImageMorphing.dir/JUCE/modules/juce_gui_basics/juce_gui_basics.mm.o"
+  "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/JUCE/modules/juce_gui_extra/juce_gui_extra.mm" "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/build/CMakeFiles/ImageMorphing.dir/JUCE/modules/juce_gui_extra/juce_gui_extra.mm.o"
+  "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/Source/BlendingImageComponent.cpp" "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/build/CMakeFiles/ImageMorphing.dir/Source/BlendingImageComponent.cpp.o"
+  "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/Source/Main.cpp" "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/build/CMakeFiles/ImageMorphing.dir/Source/Main.cpp.o"
+  "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/Source/MainComponent.cpp" "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/build/CMakeFiles/ImageMorphing.dir/Source/MainComponent.cpp.o"
+  "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/Source/MorphingAnimationComponent.cpp" "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/build/CMakeFiles/ImageMorphing.dir/Source/MorphingAnimationComponent.cpp.o"
+  "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/Source/PaintImageComponent.cpp" "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/build/CMakeFiles/ImageMorphing.dir/Source/PaintImageComponent.cpp.o"
+  "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/Source/SliderWithLabel.cpp" "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/build/CMakeFiles/ImageMorphing.dir/Source/SliderWithLabel.cpp.o"
+  "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/Source/WarpingImageComponent.cpp" "/Users/tuijida/Desktop/OpenGL_Course/ImageMorphing/build/CMakeFiles/ImageMorphing.dir/Source/WarpingImageComponent.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "JUCE_APPLICATION_NAME_STRING=\"\""
+  "JUCE_APPLICATION_VERSION_STRING=\"0.0.1\""
+  "JUCE_GLOBAL_MODULE_SETTINGS_INCLUDED=1"
+  "JUCE_MODULE_AVAILABLE_juce_core=1"
+  "JUCE_MODULE_AVAILABLE_juce_data_structures=1"
+  "JUCE_MODULE_AVAILABLE_juce_events=1"
+  "JUCE_MODULE_AVAILABLE_juce_graphics=1"
+  "JUCE_MODULE_AVAILABLE_juce_gui_basics=1"
+  "JUCE_MODULE_AVAILABLE_juce_gui_extra=1"
+  "JUCE_STANDALONE_APPLICATION=1"
+  "JUCE_USE_CURL=0"
+  "JUCE_WEB_BROWSER=0"
+  "NDEBUG=1"
+  "_NDEBUG=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "ImageMorphing_artefacts/JuceLibraryCode"
+  "../JUCE/modules"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
